e04a6389408cc1b90982342e7d23d4550c852a45
mjbryan <mjbyran10@gmail.com>
2020-09-01 15:50:29 +0200

    added react-test-renderer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { store } from './app/store';
 import { ThemeProvider } from 'styled-components';
 import { dark } from './theme';
 import Login from './features/login/Login';
+import { selectLogin } from './features/login/loginSlice';
 
 function App() {
+   const { isAuthenticated } = useSelector(selectLogin);
+
+
    return (
       <Provider store={store}>
          <ThemeProvider theme={dark}>
-            <Login>
-               <div className="App"></div>
-            </Login>
+            {isAuthenticated ? <div>Test</div> : <Login/> }
+            
          </ThemeProvider>
       </Provider>
    );
