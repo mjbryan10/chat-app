575a7f5aae7ad9e9fbb963c2e744b81b6a809712
mjbryan <mjbyran10@gmail.com>
2020-09-07 12:02:19 +0200

    refactored List to accept and pass on message details and user details

--- src/features/message/MessageList/index.tsx
+++ src/features/message/MessageList/index.tsx
@@ -1,16 +1,33 @@
-import React, { FC } from 'react'
-import { Message } from 'Api/@types'
+import React, { FC } from 'react';
+import { Message } from 'Api/@types';
+import { Participant } from '../messageSlice';
+import MessageItem from '../MessageItem';
+import * as S from './styles';
 
 interface Props {
-  messages: Message[];
+   messages: Message[];
+   participants: Participant[];
 }
 
-const MessageList: FC<Props> = ({messages}) => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const MessageList: FC<Props> = ({ messages, participants }) => {
+   return (
+      <S.Wrapper>
+         {messages.map((message, index) => {
+            const participant = participants.filter(
+               (user) => user.id === message.senderId
+            );
+            const isChain = (message.senderId === messages[index+1].senderId);
+            return (
+               <MessageItem
+                  key={message.id}
+                  messageDetails={message}
+                  userDetails={participant}
+                  isChainMessage={isChain}
+               />
+            );
+         })}
+      </S.Wrapper>
+   );
+};
 
-export default MessageList
+export default MessageList;
