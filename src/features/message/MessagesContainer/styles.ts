9e0a34862d8843f5d6579fee8a052d7bd0a3e4d8
mjbryan <mjbyran10@gmail.com>
2020-09-09 18:03:38 +0200

    added flex styles

--- src/features/message/MessagesContainer/styles.ts
+++ src/features/message/MessagesContainer/styles.ts
@@ -3,16 +3,17 @@ import styled, { Theme, ThemeProps } from 'styled-components';
 export const Container = styled.div`
    position: relative;
    max-width: 1800px;
-   /* width: 80%; */
    width: 100%;
+   height: 100%;
+   max-height: 80vh;
    display: flex;
    flex-flow: column nowrap;
 `;
 
 export const MessagesDisplay = styled.div<ThemeProps<Theme>>`
    height: 100%;
+   flex-grow: 1;
    min-height: 200px;
-   max-height: 50vh;
    overflow-y: auto;
    padding: 1em;
    background: ${({ theme }) => theme.background.messagesDisplay};
