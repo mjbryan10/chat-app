c5c4c27be9441404736237070a6cb0e104568897
mjbryan <mjbyran10@gmail.com>
2020-09-10 21:51:05 +0200

    added jwt token to login/out processes

--- src/features/login/LogoutButton/index.tsx
+++ src/features/login/LogoutButton/index.tsx
@@ -3,12 +3,14 @@ import { logoutUser } from '../loginSlice';
 import { useDispatch } from 'react-redux';
 import ButtonNavbarIcon from 'components/ButtonNavbarIcon';
 import { IoMdPower } from 'react-icons/io';
+import Cookies from 'js-cookie';
 
 const LogoutButton = () => {
    const dispatch = useDispatch();
 
    const handleClick = () => {
       dispatch(logoutUser());
+      Cookies.remove('auth_token');
    };
    return (
       <ButtonNavbarIcon handleClick={handleClick}>
