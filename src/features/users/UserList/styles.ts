6e9dade2f6f8e28e6f513274635cba51e4063c03
mjbryan <mjbyran10@gmail.com>
2020-09-10 18:50:11 +0200

    added theme styling

--- src/features/users/UserList/styles.ts
+++ src/features/users/UserList/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
 export const ListWrapper = styled.ul`
-  list-style: none;
+   width: 100%;
+   list-style: none;
+   margin: 0;
+   padding: 0;
+   & li {
+      width: 100%;
+      margin-bottom: 5px;
+      &:first-of-type {
+         margin-top: 5px;
+      }
+   }
 `;
