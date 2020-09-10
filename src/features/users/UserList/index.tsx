960cd51265d27723af74ba5e942243f7c3ea74ca
mjbryan <mjbyran10@gmail.com>
2020-09-10 10:22:55 +0200

    changed name

---
+++ src/features/users/UserList/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import { User } from 'shared/Api/types';
+import UserItem from '../UserItem';
+import * as S from './styles';
+
+interface Props {
+   users: User[];
+   handleItemClick?: (id: number) => void;
+}
+
+const UserList: FC<Props> = ({ users, handleItemClick }) => {
+   const onItemClick = (id: number) => {
+      if (handleItemClick) handleItemClick(id);
+   };
+   return (
+      <S.ListWrapper>
+         {users.map((user) => (
+            <li>
+               <UserItem key={user.id} user={user} handleClick={onItemClick} />
+            </li>
+         ))}
+      </S.ListWrapper>
+   );
+};
+
+export default UserList;
