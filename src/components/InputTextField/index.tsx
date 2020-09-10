3b542efdf639cbb7234d0cb5ca9626a7c900e8f5
mjbryan <mjbyran10@gmail.com>
2020-09-10 10:46:53 +0200

    reformatted

--- src/components/InputTextField/index.tsx
+++ src/components/InputTextField/index.tsx
@@ -1,29 +1,36 @@
 import React, { useState, FC } from 'react';
-import * as S from './styles'
+import * as S from './styles';
 
 export interface Props {
    disabled?: boolean;
    placeholder?: string;
    initialValue?: string;
-   handleChange?: (value:string) => void;
+   handleChange?: (value: string) => void;
 }
 
 const InputTextField: FC<Props> = ({
    disabled = false,
    placeholder = '',
    initialValue = '',
-   handleChange
+   handleChange,
 }) => {
    const [value, setValue] = useState(initialValue);
 
    const onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
       event.preventDefault();
-      const {value} = event.currentTarget;
+      const { value } = event.currentTarget;
       if (disabled) return;
       setValue(value);
       if (handleChange) handleChange(value);
    };
-   return <S.Input value={value} onChange={onChange} placeholder={placeholder} data-testid="input-text-field" />;
+   return (
+      <S.Input
+         value={value}
+         onChange={onChange}
+         placeholder={placeholder}
+         data-testid="input-text-field"
+      />
+   );
 };
 
 export default InputTextField;
